87537104a35bd305360f703afda9c2cf37f35cb4
Stuart <crocodilestu@users.noreply.github.com>
2023-05-24 13:11:00 +1000

    Update app.py
    Adding SSL

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, Response
 import requests
+import ssl
 
 app = Flask(__name__)
 
@@ -9,10 +10,12 @@ def proxy(path):
     url = f"http://{request.host}/{path}"
     headers = dict(request.headers)
     headers['Host'] = request.host
-    response = requests.get(url, headers=headers, stream=True)
+    response = requests.get(url, headers=headers, stream=True, verify=False)
     excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
     headers = [(name, value) for (name, value) in response.raw.headers.items() if name.lower() not in excluded_headers]
     return Response(response.content, response.status_code, headers)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    context = ssl.SSLContext(ssl.PROTOCOL_TLS)
+    context.load_cert_chain('/ssl/fullchain.pem', '/ssl/privkey.pem')
+    app.run(host='0.0.0.0', ssl_context=context)
